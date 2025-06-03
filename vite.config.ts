7a608c5bfaa073ed6fee83fad5f349235891cb46
Abegail Hornilla <Aby@Aby-MacBook-Pro.local>
2025-06-03 23:27:33 +0800

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/abegailhornilla-portfolio-website/", // 👈 add your GitHub repo name here
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
